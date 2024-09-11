12756b150a1a47178295e40b2b240d3d46574935
Jotape612 <joao.p.sousa7@ba.estudante.senai.br>
2024-09-11 10:59:57 -0300

    Implementação do código e inicio da aba Carrinho

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function exemplo_alerta(){
+    alert("Boa aula!");
+  }
+
+  var a, b, c;
+  a = "Oi";
+  a = 12;
+  b = a + 2.5;
+  c = b + "5";
+  
+  document.write (typeof(a), "", typeof(b), "" , typeof(c));
