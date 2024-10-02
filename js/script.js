97b4dd1975ea7d9f76ba19ccb78645646fe3b166
João Paulo Oliveira <158469595+Jotape612@users.noreply.github.com>
2024-10-02 11:25:35 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,97 @@
-function exemplo_alerta(){
-    alert("Boa aula!");
-  }
-
-  var a, b, c;
-  a = "Oi";
-  a = 12;
-  b = a + 2.5;
-  c = b + "5";
+function addToCart(event) {
+  alert("Item foi adicionado ao carrinho");
   
-  document.write (typeof(a), "", typeof(b), "" , typeof(c));
+  const card = event.target.closest('.card');
+
+  const productName = card.querySelector('.card-name').textContent;
+  const productPrice = card.querySelector('.real').textContent;
+  const productImage = card.querySelector('.produto').src;
+
+  const productData = {
+    name: productName,
+    price: productPrice,
+    image: productImage
+  };
+
+  
+  const cartProducts = JSON.parse(localStorage.getItem('cartProducts')) || [];
+  cartProducts.push(productData);
+
+
+  localStorage.setItem('cartProducts', JSON.stringify(cartProducts));
+
+  window.location.href = 'compras.html';
+}
+
+function loadCart() {
+  const cartProducts = JSON.parse(localStorage.getItem('cartProducts'));
+
+  if (cartProducts) {
+
+    const cartList = document.getElementById('cart');
+    cartList.innerHTML = '';
+
+    cartProducts.forEach((product) => {
+
+      const listItem = document.createElement('li');
+      listItem.classList.add('lista-nova');
+      
+      const productInfo = document.createElement('div');
+      productInfo.classList.add('div-nova');
+      productInfo.className = 'product-info';
+
+      const productImage = document.createElement('img');
+      productImage.classList.add('img-nova');
+      productImage.src = product.image;
+      productImage.alt = 'imagem do produto';
+      productInfo.appendChild(productImage);
+
+      const productName = document.createElement('p');
+      productName.classList.add('nome-novo');
+      productName.textContent = product.name;
+
+      const productPrice = document.createElement('p');
+      productPrice.classList.add('preco-novo');
+      productPrice.textContent = product.price;
+
+      const buttonGroup = document.createElement('div');
+      buttonGroup.classList.add('botao-novo');
+      buttonGroup.className = 'button-group';
+
+      const decreaseBtn = document.createElement('button');
+      decreaseBtn.textContent = '-';
+      decreaseBtn.classList.add('decrease-novo');
+
+      const itemQnt = document.createElement('input');
+      itemQnt.classList.add('input-novo');
+      itemQnt.type = 'number';
+      itemQnt.min = '1';
+      itemQnt.max = '100';
+      itemQnt.value = '1';
+      itemQnt.readOnly = true;
+
+      const increaseBtn = document.createElement('button');
+      increaseBtn.classList.add('increase-novo');
+      increaseBtn.textContent = '+';
+
+      const removeText = document.createElement('p');
+      removeText.className = 'remove-text';
+      removeText.textContent = 'Remover';
+
+
+      buttonGroup.appendChild(decreaseBtn);
+      buttonGroup.appendChild(itemQnt);
+      buttonGroup.appendChild(increaseBtn);
+      buttonGroup.appendChild(removeText);
+
+
+      listItem.appendChild(productInfo);
+      listItem.appendChild(productName);
+      listItem.appendChild(productPrice);
+      listItem.appendChild(buttonGroup);
+      cartList.appendChild(listItem);
+    });
+  }
+}
+
+window.onload = loadCart;
